45e7ab27d36a19fac832099ab26154805f69ffbd
Sayyed Ali Tayyeb <salitayyeb@gmail.com>
2022-05-26 19:14:51 +0430

    implement the basic algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 class Main {
-    private static int numberOfVariables = 4; //default value for testing
+    private static int numberOfVariables;
 
     public static void main(String[] args) {
         //scan the input data
@@ -16,8 +14,61 @@ class Main {
         for (String string : inputString.split(","))
             mintermNumbers.add(Integer.parseInt(string));
 
-        //start the algorithm
-        //TODO
+        //Run the algorithm and print results
+        printSOP(getSimplifiedSOP(mintermNumbers));
+    }
+
+    //Returns a list of simplified minterms
+    private static List<String> getSimplifiedSOP(List<Integer> mintermNumbers) {
+        //convert the minterms to binary form
+        List<String> mintermBinaries = new ArrayList<>();
+        for (int number : mintermNumbers)
+            mintermBinaries.add(getBinary(number));
+
+        //loop over the main algorithm
+        while (!algorithm(mintermBinaries).containsAll(mintermBinaries))
+            mintermBinaries = algorithm(mintermBinaries);
+
+        //convert from binary to letter form
+        List<String> minterms = new ArrayList<>();
+        for (String binary : mintermBinaries)
+            minterms.add(getMinterm(binary));
+        return minterms;
+    }
+
+    //The main algorithm
+    private static List<String> algorithm(List<String> mintermBinaries) {
+        List<String> newMinterms = new ArrayList<>();
+        int size = mintermBinaries.size();
+        boolean[] selected = new boolean[size];
+        //find all Grey Codes
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (isGrey(mintermBinaries.get(i), mintermBinaries.get(j))) {
+                    selected[i] = selected[j] = true;
+                    String newBinary = replaceComplements(mintermBinaries.get(i), mintermBinaries.get(j));
+                    if (!newMinterms.contains(newBinary))
+                        newMinterms.add(newBinary);
+                }
+            }
+        }
+        //add all not selected minterms
+        for (int i = 0; i < size; i++)
+            if (!selected[i])
+                newMinterms.add(mintermBinaries.get(i));
+        return newMinterms;
+    }
+
+    //Print the results in the terminal
+    private static void printSOP(List<String> minterms) {
+        boolean firstFlag = true;
+        for (String minterm : minterms) {
+            if (!firstFlag)
+                System.out.print(" + ");
+            else
+                firstFlag = false;
+            System.out.print(minterm);
+        }
     }
 
     //Returns the possible letters. For example:
@@ -68,9 +119,11 @@ class Main {
         List<String> letters = getVars();
         StringBuilder minterm = new StringBuilder();
         for (int i = 0; i < binary.length(); i++) {
-            minterm.append(letters.get(i));
-            if (binary.charAt(i) == '0')
-                minterm.append("'");
+            if (binary.charAt(i) != '-') {
+                minterm.append(letters.get(i));
+                if (binary.charAt(i) == '0')
+                    minterm.append("'");
+            }
         }
         return String.valueOf(minterm);
     }
